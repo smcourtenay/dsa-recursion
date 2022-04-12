e21badb33fd501cf3ec46fa3330bab242da22263
hectorchu~ <hector.chu828@gmail.com>
2022-04-12 12:25:10 -0700

    Stuck on findIndex

--- recursion.js
+++ recursion.js
@@ -30,14 +30,28 @@ function everyOther(str) {
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
+
 function isPalindrome(str) {
+    if(str.length === 0 || str.length === 1) return true;
+
+    const left = str[0];
+    const right = str[str.length - 1];
+
+    return left === right ? isPalindrome(str.slice(1, str.length - 1)) : false; 
 
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
-
-function findIndex(arr, val) {
-
+[4, 1, 2, 3, 4]
+[4, 1, 2, 3] 4
+[4, 1, 2] 3, 4
+[4, 1] 2 3 4
+[4] 1 2 3 4
+[] 4 1 2 3 4
+ function findIndex(arr, val) {
+    if(arr.length === 0) return -1;
+    // const idx = (arr[arr.length - 1] === val) ? arr.length - 1 : -1;
+    return (arr[arr.length - 1] === val) ? idx : findIndex(arr.slice(arr.length - 1, val));
 }
 
 /** revString: return a copy of a string, but in reverse. */
